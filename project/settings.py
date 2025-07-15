94bd251d2ce4ae3d29dd3357cbcca0c1a8723728
kelver <kelverwt@gmail.com>
2025-07-15 19:36:33 -0300

    Ajuste DATABASE_URL para ambiente Heroku

--- project/settings.py
+++ project/settings.py
@@ -79,7 +79,7 @@ WSGI_APPLICATION = 'project.wsgi.application'
 
 DATABASES = {
     'default': dj_database_url.config(
-        default=config('DATABASE_URL')
+        default=os.environ.get('DATABASE_URL', 'sqlite:///db.sqlite3')
     )
 }
 
